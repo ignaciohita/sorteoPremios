a76468c6d944bba587d05e0ad236d484b7b36a98
ignaciohita <ignaciohita@gmail.com>
2015-05-25 11:29:07 +0200

    Initial commit

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,7 +1,9 @@
-/*global require*/
+/*global require, console*/
 var gulp = require("gulp"),
     webserver = require("gulp-webserver"),
-    jshint = require("gulp-jshint");
+    jsHint = require("gulp-jshint"),
+    jsLint = require("gulp-jslint"),
+    gjsLint = require("gulp-gjslint");
 
 // Servidor web de desarrollo
 gulp.task("dev-server", function () {
@@ -12,6 +14,37 @@ gulp.task("dev-server", function () {
             open: true,
             livereload: true
         }));
+});
+
+// Busca errores de JavaScript de acuerdo con JsHint
+gulp.task("jsHint", function () {
+    "use strict";
+
+    return gulp.src(["./app/js/**/*.js"])
+        .pipe(jsHint(".jshintrc"))
+        .pipe(jsHint.reporter("default"));
+});
+
+// Busca errores de JavaScript de acuerdo con JsLint
+gulp.task("jsLint", function () {
+    "use strict";
+
+    return gulp.src(["./app/js/**/*.js"])
+        .pipe(jsLint())
+        .on("error", function (error) {
+            console.error(String(error));
+        });
+});
+
+// Busca errores de JavaScript de acuerdo con Google JsLinter
+gulp.task("jsGoogleLint", function () {
+    "use strict";
+
+    return gulp.src(["./app/js/**/*.js"])
+        .pipe(gjsLint())
+        .pipe(gjsLint.reporter("console"), {
+            fail: true
+        });
 });
 
 gulp.task("default", ["dev-server"]);
